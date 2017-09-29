a39a5e1ecb66b55c188239d33bf7fae1461f70d3
ejkaiser <ej@jellyvision.com>
2017-09-29 17:53:40 -0500

    Update license, make errors recoverable

--- plugins/mermaid.js
+++ plugins/mermaid.js
@@ -2,7 +2,7 @@ const doctrine = require('doctrine');
 
 exports.handlers = {
   newDoclet: function(e) {
-    let graphvizComment = doctrine.parse([e.doclet.comment].join('\n'), { unwrap: true, tags: ['graphviz'] }).tags;
+    let graphvizComment = doctrine.parse([e.doclet.comment].join('\n'), { unwrap: true, tags: ['graphviz'], recoverable: true }).tags;
     e.doclet.description = e.doclet.description || '';
     e.doclet.description = '<script type="text/javascript" src="https://unpkg.com/mermaid@7.1.0/dist/mermaid.min.js"/>' + e.doclet.description;
     for (let i = 0; i < graphvizComment.length; i++) {
