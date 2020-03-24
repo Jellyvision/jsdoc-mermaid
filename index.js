3a4750077f01aba9142085785fab72597f6abe67
vlnk <vlnk@protonmail.com>
2020-03-23 22:09:01 -0400

    🎨 Add constants to make version switching easier

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const doctrine = require('doctrine')
 
-const escapeHtmlChar = {
+/**
+ * @constant {string} JSDOC_MERMAID_TAG
+ * @description Matches starting strings of a jsdoc comment
+ */
+const JSDOC_MERMAID_TAG = /@mermaid\b/
+
+/**
+ * @constant {string} MERMAID_VERSION
+ * @description Semver Mermaid version
+ */
+const MERMAID_VERSION = '8.4.8'
+
+/**
+ * @constant {string} MERMAID_HTML_SCRIPT
+ * @description Html tag that include mermaid library
+ */
+const MERMAID_HTML_SCRIPT = `<script src="https://unpkg.com/mermaid@${MERMAID_VERSION}/dist/mermaid.min.js"></script>`
+
+/**
+ * @constant {Object} ESCAPE_HTML_MAPPING
+ * @description Map of all characters that need HTML escaping
+ */
+const ESCAPE_HTML_MAPPING = {
   '&': '&amp;',
   '<': '&lt;',
   '>': '&gt;',
@@ -9,7 +31,7 @@ const escapeHtmlChar = {
 }
 
 function escapeHtml(str) {
-  return str.replace(/[&<>"']/g, c => escapeHtmlChar[c])
+  return str.replace(/[&<>"']/g, c => ESCAPE_HTML_MAPPING[c])
 }
 
 let isAddedMermaid = {}
